bebfcdd3b5d0e32cba2ef086979f15ba4806d362
rolandmarics <ro9810@gmail.com>
2024-09-17 15:30:32 +0200

    k

--- script.js
+++ script.js
@@ -1,3 +1,70 @@
-let firstName = "Roland"
-let lastName = "Marics"
-console.log(firstName + " " +lastName);
+let message = "Hi my name is Martin"
+console.log(message)
+
+let myname = "Roland";
+ let message1 = "Hi my nema is " +myname
+ console.log(message1)
+ 
+let age ="25"
+let message2 ="Hi my name is "  +myname+ " and I am " +age+ " years old."
+console.log(message2)
+
+
+
+let players = ["martin", "Thomas", "Peter","Mathias","Niki"];
+console.log(players)
+console.log(`The most valuable player of the match is ${players[2]}`)
+
+
+
+let cars = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrar"]
+console.log(cars)
+
+let Cars = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrar"]
+Cars.sort();
+console.log(Cars)
+
+
+
+let fruits = ['apple', 'banana', 'kiwi', 'mango', 'pear']
+console.log(fruits)
+
+fruits.splice(5,0,"orange")
+console.log(fruits)
+
+fruits.shift();
+console.log(fruits)
+
+fruits.pop();
+fruits.unshift("apple")
+console.log(fruits)
+
+fruits.splice(0,1 ,"strawberry")
+console.log(fruits);
+
+
+
+let animals = ['monkey', 'horse', 'dog', 'elephant', 'giraffe']
+console.log(animals);
+
+animals.shift()
+console.log(animals);
+
+animals.unshift("cat")
+console.log(animals);
+
+animals.pop()
+console.log(animals);
+
+animals.splice(4,0, "tiger")
+console.log(animals);
+
+
+
+
+
+
+
+
+
+
